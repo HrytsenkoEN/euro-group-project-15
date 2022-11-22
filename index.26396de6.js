ee465edebfef6e35fa55df1c5078be41a71efb25
HrytsenkoEN <111355863+HrytsenkoEN@users.noreply.github.com>
2022-11-22 00:49:56 +0000

    Deploying to gh-pages from @ HrytsenkoEN/euro-group-project-15@26e954e0a72c989003f5cde9453ffba193df1d74 🚀

---
+++ index.26396de6.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.26396de6.js.map
---
+++ index.beefc994.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0}});
+//# sourceMappingURL=index.beefc994.js.map
